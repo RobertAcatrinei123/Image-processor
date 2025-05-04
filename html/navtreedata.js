ddbc9992bd38307d6705a13f1d1f7054209333c5
Acatrinei Robert-Marian <r.acatrinei2005@gmail.com>
2025-05-04 18:04:29 +0300

    changed docs name

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "ImageProcessor", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
